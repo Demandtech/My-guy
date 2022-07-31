a790380298b1d7392c2cc17d21b0680f6fb7336e
Demandtech <demandwork247@gmail.com>
2022-07-31 06:11:34 +0100

    okay

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var i = 0;
-var txt =
-    "We specialize in the repair and selling of phones, Laptops, games, and all Gadgets."; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
+var txt = "AdeTech Venture "; /* The text */
+var speed = 150; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
     if (i < txt.length) {
